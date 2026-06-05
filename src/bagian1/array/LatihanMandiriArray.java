3a73da0ccfbfb0b58faecd1009f6c6ea029cec03
Dir <haidiraditya16@gmail.com>
2026-06-05 13:35:37 +0800

    Initial commit

---
+++ src/bagian1/array/LatihanMandiriArray.java
@@ -0,0 +1,29 @@
+package bagian1.array;
+
+public class LatihanMandiriArray {
+
+    public static void main(String[] args) {
+        // Latihan array suhu
+        double[] suhu = {80, 75, 90, 60, 85, 56};
+        System.out.println("Suhu Tertinggi : " + suhu[2]);
+        
+        // Latihan array hari
+        String[] hari = {"senin", "selasa", "rabu", "kamis", "jumat"};
+        for (String kata : hari) {
+            if(kata.length() > 5){
+                System.out.println(kata);
+            }
+        }
+        
+        // Latihan hitung angka genap
+        int[] angka = {4, 8, 15, 16, 23, 42};
+        int jumlah_genap = 0;
+        for (int i : angka) {
+            if (i % 2 == 0) {
+                System.out.println(i + " adalah genap");
+                jumlah_genap++;
+            }
+        }
+        System.out.println("Angka Genap ada : " + jumlah_genap);
+    }
+}
